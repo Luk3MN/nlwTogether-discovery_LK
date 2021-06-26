398ffa9c1950e2e7d8fb8a65b90f2ca4043d8cb5
Lucas Nunes <55895056+luk3mn@users.noreply.github.com>
2021-06-26 13:54:40 -0300

    Aula 05 – Aprofundando no SQL

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,55 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
 
-    for (var i = 0; i < 6; i++) {
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-      roomId += Math.floor(Math.random() * 10).toString()
-    }
+    while (isRoom) {
+      
+      /* Gera o numero da sala */
+      for (var i = 0; i < 6; i++) {
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+        roomId += Math.floor(Math.random() * 10).toString()
+      } 
 
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
+      /* Verifica se o numero ja existe */
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId)
+      if (!isRoom) {
+        /* Inseri a sala no banco */
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+
+    }  
 
     await db.close(); // fecha a conexao com o banco
+
+    res.redirect(`/room/${roomId}`)
+  },
+
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 1`)
+    let isNoQuestions
+
+    if (questions.length == 0) {
+      if (questionsRead.length == 0) {
+        isNoQuestions = true
+      }
+    }
+
+    res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
+  },
+
+  enter(req, res) {
+    const roomId = req.body.roomId
 
     res.redirect(`/room/${roomId}`)
   }
